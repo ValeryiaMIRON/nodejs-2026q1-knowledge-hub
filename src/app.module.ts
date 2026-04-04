fa4eb05d8589f56d148945ffd9ca8989dcf564e0
Valeryia <vmiron@exadel.com>
2026-04-04 14:54:52 +0400

    feat: add request logging middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,6 +6,7 @@ import { UserModule } from './user/user.module';
 import { ArticleModule } from './article/article.module';
 import { CategoryModule } from './category/category.module';
 import { CommentModule } from './comment/comment.module';
+import { RequestLoggerMiddleware } from './common/middleware/request-logger.middleware';
 import { StorageModule } from './storage/storage.module';
 @Module({
   imports: [
@@ -19,4 +20,8 @@ import { StorageModule } from './storage/storage.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(RequestLoggerMiddleware).forRoutes('*');
+  }
+}
